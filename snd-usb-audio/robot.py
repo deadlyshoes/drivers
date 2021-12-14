74c47171317d3c2c537fc17a82febcf1a0e76924
deadlyshoes <khoseilmar@gmail.com>
2021-12-14 16:48:39 -0300

    Update robot.py

--- snd-usb-audio/robot.py
+++ snd-usb-audio/robot.py
@@ -4,15 +4,18 @@ import sys
 def main():
     efeitos = {'normal': '0', 'robo': '1', 'alien': '2'}
 
-    try:    
-        efeito = sys.argv[1]
-        print("Efeito aplicado")
+    try:
+        efeito = efeitos[sys.argv[1]]
+
     except:
         print(" Passe um dos parametros abaixo,\n para selecionar um efeito:\n")
         print("- normal;")
         print("- robo;")
         print("- alien.")
+        sys.exit()
 
+    with open("/sys/module/snd_usb_audio/parameters/efeito", "w") as efeito_file:
+        efeito_file.write(efeito)
 
 if __name__ == "__main__":
     main()
